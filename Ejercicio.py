31caea96a8d2a4453e4b934f60aa28720f1e6eb7
n4ncy27 <liliana58466@gmail.com>
2022-10-04 16:15:32 -0500

    DESARROLLO..

--- Ejercicio.py
+++ Ejercicio.py
@@ -1,7 +1,7 @@
 # Ejercicio:una pelota se deja caer de desde un altura H
 #  y en cada revote sube el 10% menos de la anterior hacer el diagrama flujo y
 #  el programa en python, que lea H y que calcule e imprima en cual
-#  revote la pelota no alcanza a subir la quinta parte de la altura inicial.
+#  revote la pelota no alcanza a subir la quinta parte de la altura inicial.-
 
 print ("-----------------------")
 print ("----------Revote-------")
